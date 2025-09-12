1e126c179e79b8f5236c10ae58cc355e865c5b99
Ashish Tiwari <66666903+ashish9731@users.noreply.github.com>
2025-09-12 09:58:34 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./route";
+import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
